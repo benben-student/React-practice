83e397762970f4a7fa84c35186cfbae514602471
520-forever <a441832620@126.com>
2022-02-23 17:15:34 +0800

    new add

---
+++ -09.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+class XiaojiejieItem extends Component {
+    constructor(props) {
+        super(props)
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    render() {
+        return (
+            <div>
+                <li onClick={this.handleClick}>{this.props.avname}为你服务-{this.props.content}</li>
+            </div>
+        );
+    }
+    handleClick() {
+        this.props.deleteItem(this.props.index)
+    }
+}
+XiaojiejieItem.propTypes={
+       content:PropTypes.string,
+       index:PropTypes.number,
+       deleteItem:PropTypes.func,
+       avname:PropTypes.string
+}
+XiaojiejieItem.defaultProps={
+    avname:'川岛枫'
+}
+
+export default XiaojiejieItem;
