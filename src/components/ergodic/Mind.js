3a416396bb2fb717e420b5371f81426dfa501d00
benben-student <a441832620@126.com>
2022-07-28 23:06:58 +0800

    add

--- src/components/ergodic/Mind.js
+++ src/components/ergodic/Mind.js
@@ -1,19 +1,35 @@
 import React from "react";
-class Mind extends React.Component{
-    constructor(props){
+class Mind extends React.Component {
+    constructor(props) {
         super(props)
-        this.state={
-            list:[
-                'A','B','C'
-            ]
+        this.state = {
+            list: [
+                'A', 'B', 'C'
+            ],
+            indexs: 0
         }
     }
-    render(){
-        const {list}=this.state
-        return(
+    onMouseOver(index) {
+        this.setState({
+            indexs: index
+        })
+        console.log(this.state.indexs);
+    }
+    onMouseOut() {
+        const p = document.getElementsByClassName('menu')[0];
+        p.className = ''
+    }
+    render() {
+        const { list, indexs } = this.state
+        return (
             <div>
-                {list.map((item,index)=>{
-                    return <li className="span" key={index}>{item}</li>
+                {list.map((item, index) => {
+                    return <li
+                        onMouseOut={this.onMouseOut.bind(this)}
+                        className={indexs === index ? 'menu' : ''}
+                        onMouseOver={this.onMouseOver.bind(this, index)}
+                        key={index}>{item}
+                    </li>
                 })}
             </div>
         )
