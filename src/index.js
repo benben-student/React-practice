94133ea8b7e3b51c1976369855063948b5ddc6eb
benben-student <a441832620@126.com>
2022-08-06 19:14:17 +0800

    add

--- src/index.js
+++ src/index.js
@@ -7,9 +7,14 @@ import ReactDOM from 'react-dom/client';
 
 // import AppRouter from './JDrouter/router/AppRouter'
 
-import AppRouter from './UI-frame/router/AppRouter';
-import "./index.css"
+// import AppRouter from './UI-frame/router/AppRouter';
+// import "./index.css"
+
 // import Http from './component/Http';
+import './index.css'
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import App from './stu-shop-manager/App'
+import { mainRouters } from './stu-shop-manager/routers/index';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -17,8 +22,21 @@ root.render(
 
     {/* <AppRouter /> */}
 
-    <AppRouter />
+    {/* // <AppRouter /> */}
     {/* <Http/> */}
+    <Router>
+      <Switch>
+        <Route path="/admin" render={routeProps=><App {...routeProps}/>}/>
+        {
+          mainRouters.map(route => {
+            return <Route key={route.path} {...route}></Route>
+          })
+        }
+      <Redirect to="/404"></Redirect>
+      </Switch>
+    </Router>
+
+
   </React.StrictMode>
 );
 
---
+++ src/stu-shop-manager/routers/index.js
@@ -0,0 +1,31 @@
+import List from "../Pages/admin/product/List"
+import  Index from "../Pages/admin/dashboard/Index"
+import Edit from "../Pages/admin/product/Edit"
+import Login from "../Pages/Login"
+import PageNotFound from "../Pages/PageNotFound"
+
+export const mainRouters = [{
+    path: '/login',
+    component: Login
+},{
+    path:'/404',
+    component:PageNotFound
+}
+]
+export const admirnRouters = [{
+    path: '/admin/dashboard',
+    component: Index,
+    isShow:true,
+    title:"看板"
+}, {
+    path: '/admin/product',
+    component: List,
+    isShow:true,
+    exact:true,
+    title:"商品管理"
+}, {
+    path: '/admin/product/edit/:id',
+    isShow:false,
+    component: Edit
+}
+]
