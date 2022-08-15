f4e610f2f408a65ad977376b440140012e8f7fa1
benben-student <a441832620@126.com>
2022-08-15 19:37:53 +0800

    feat:copy to clipboard

--- src/ref/Refs.js
+++ src/ref/Refs.js
@@ -10,15 +10,44 @@ class CustomTextInput extends React.Component {
     }
     getInputValue = () => {
         console.log(this.myRefs.current.value);
+        const a = document.getElementById("input")
+        console.log(a.value);
     }
+
     render() {
         return (
             <div>
-                <input type="text" value="123" ref={this.myRefs} />
+                <input id="inputs" type="text" value="123" ref={this.myRefs} />
                 <input type="button" value="button" onClick={this.getInputValue} />
             </div>
         )
     }
 }
 
-export default CustomTextInput
+
+
+class CopyInputVlaue extends React.Component {
+    constructor(props) {
+        super(props)
+        this.myInputValue = React.createRef()
+    }
+    CopyValue = (text) => {
+        // const Url=document.getElementById("input")
+        // Url.select()
+        // document.execCommand("Copy")
+        const inputValue = this.myInputValue.current.value
+        const copyInput = document.getElementById("input")
+        copyInput.setAttribute("value", inputValue)
+        copyInput.select()
+        document.execCommand('copy');
+    }
+    render() {
+        return (
+            <div>
+                <input id="input" value="github//www.github.com" ref={this.myInputValue} />
+                <button onClick={this.CopyValue}>点击复制链接</button>
+            </div>
+        )
+    }
+}
+export default CopyInputVlaue
